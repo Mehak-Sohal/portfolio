d6af76499d99f7b4c1c8afba73dc06a411739cac
mehak sohal <mehak.sohal94@gmail.com>
2021-04-19 12:01:41 -0400

    update resume

--- src/Resume.js
+++ src/Resume.js
@@ -1,55 +1,55 @@
-import React from 'react';
-import './stylesheet.css';
+import React from "react";
+import "./stylesheet.css";
 
 const Resume = () => {
   return (
-    <section id='resume'>
-      <div className='resume'>
-        <div className='resume-section'>
-          <div className='resume-title'>
+    <section id="resume">
+      <div className="resume">
+        <div className="resume-section">
+          <div className="resume-title">
             <h4>EDUCATION</h4>
           </div>
-          <div className='resume-info'>
-            <p className='resume-infoLineOne'>University of Windsor</p>
-            <p className='resume-infoLineTwo'>
+          <div className="resume-info">
+            <p className="resume-infoLineOne">University of Windsor</p>
+            <p className="resume-infoLineTwo">
               Masters in Electrical and Computer Engineering
-              <span className='time-span'>(2018-2019)</span>
+              <span className="time-span">(2018-2019)</span>
             </p>
-            <p className='resume-infoLineThree'>Graduated with A grade</p>
+            <p className="resume-infoLineThree">Graduated with A grade</p>
 
-            <p className='resume-infoLineOne'>Guru Nanak Dev University</p>
-            <p className='resume-infoLineTwo'>
+            <p className="resume-infoLineOne">Guru Nanak Dev University</p>
+            <p className="resume-infoLineTwo">
               Bachelors in Technology - Electronics and Communication System
               Engineering
-              <span className='time-span'>(2013-2017)</span>
+              <span className="time-span">(2013-2017)</span>
             </p>
-            <p className='resume-infoLineThree'>Graduated with A grade</p>
+            <p className="resume-infoLineThree">Graduated with A grade</p>
           </div>
         </div>
 
-        <div className='resume-section section2'>
-          <div className='resume-title'>
+        <div className="resume-section section2">
+          <div className="resume-title">
             <h4>WORK EXPERIENCE</h4>
           </div>
-          <div className='resume-info'>
-            <p className='resume-infoLineOne'>Software Developer</p>
-            <p className='resume-infoLineTwo'>
+          <div className="resume-info">
+            <p className="resume-infoLineOne">Software Developer</p>
+            <p className="resume-infoLineTwo">
               Omada Tek, Toronto, CA
-              <span className='time-span'>(Nov2020- Present)</span>
+              <span className="time-span">(Nov2020- Present)</span>
             </p>
-            <p className='resume-infoLineThree'>
+            <p className="resume-infoLineThree">
               Assists in both B2B and B2C projects.
               <br />
-              Design Front-End with Bootstrap, ReactJs, NextJs and other
-              development technologies.
+              Design Front-End for web and mobile apps using Bootstrap, ReactJs,
+              NextJs and other development technologies.
             </p>
 
-            <p className='resume-infoLineOne'>Frontend Web Developer</p>
-            <p className='resume-infoLineTwo'>
-              Freelancing
-              <span className='time-span'>(Sep2019- May2020)</span>
+            <p className="resume-infoLineOne">Software Developer</p>
+            <p className="resume-infoLineTwo">
+              Unicode Software Solutions Inc., Ontario, CA
+              <span className="time-span">(June2019- Oct2020)</span>
             </p>
-            <p className='resume-infoLineThree'>
+            <p className="resume-infoLineThree">
               Worked on wide variety of projects including UI/UX designs and
               layouts, designing web applications from scratch and debugging
               using latest technology, thus refining development skill.
@@ -58,12 +58,12 @@ const Resume = () => {
               restaurants, interior designers, fashion designers and Gymnasiums.
             </p>
 
-            <p className='resume-infoLineOne'>Frontend Web Developer</p>
-            <p className='resume-infoLineTwo'>
+            <p className="resume-infoLineOne">Frontend Web Developer</p>
+            <p className="resume-infoLineTwo">
               Smartcool Systems Pvt Ltd
-              <span className='time-span'>(Dec2016- Nov2017)</span>
+              <span className="time-span">(Dec2016- Nov2017)</span>
             </p>
-            <p className='resume-infoLineThree'>
+            <p className="resume-infoLineThree">
               Design website for the company highlighting different products
               company deals with.
               <br />
@@ -71,12 +71,12 @@ const Resume = () => {
               finalize the appearance of the product
             </p>
 
-            <p className='resume-infoLineOne'>Intern-Networking(CCNA)</p>
-            <p className='resume-infoLineTwo'>
+            <p className="resume-infoLineOne">Intern-Networking(CCNA)</p>
+            <p className="resume-infoLineTwo">
               NIIT, India
-              <span className='time-span'>(Jan2017- May2017)</span>
+              <span className="time-span">(Jan2017- May2017)</span>
             </p>
-            <p className='resume-infoLineThree'>
+            <p className="resume-infoLineThree">
               Ability to install, configure, operate and troubleshoot
               medium-size route and switched networks, including implementation
               and verification of connections to remote sites in a WAN.
@@ -84,49 +84,49 @@ const Resume = () => {
           </div>
         </div>
 
-        <div className='resume-section'>
-          <div className='resume-title'>
+        <div className="resume-section">
+          <div className="resume-title">
             <h4>SKILLS</h4>
           </div>
-          <div className='resume-info'>
-            <div className='resume-skillsSet'>
-              <div className='resume-skillBox'>
+          <div className="resume-info">
+            <div className="resume-skillsSet">
+              <div className="resume-skillBox">
                 <h4>ReactJs</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4> Javascript</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>Redux</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>Context API</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>Firebase</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>MongoDb</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>NodeJs</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>HTML5</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>CSS3</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>Git</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>Github</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>MySQL</h4>
               </div>
-              <div className='resume-skillBox'>
+              <div className="resume-skillBox">
                 <h4>Adobe Creative Suite</h4>
               </div>
             </div>
